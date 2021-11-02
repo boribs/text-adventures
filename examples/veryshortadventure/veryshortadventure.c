4642d0159eaca7ca1de42949d81a85cf0a055c63
boribs <christumer@gmail.com>
2021-11-01 22:06:52 -0600

    Created examples folder
    ...with an example

---
+++ examples/veryshortadventure/veryshortadventure.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "../src/common.h"
+#include "../src/adventure.h"
+
+int main() {
+    struct Adventure a;
+    if (!load_adventure("adventure.adv", &a)) {
+        printf("Error opening adventure\n");
+        return 1;
+    }
+
+    show_adventure_data(&a);
+    show_current_section(&a);
+
+    while (!end_of_adventure(&a)) {
+        get_input(&a);
+    }
+
+    return 0;
+}
