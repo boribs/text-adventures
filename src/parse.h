ed8c2a2e94f921b261d4d65e68fb9b13d6e3906d
boribs <christumer@gmail.com>
2021-11-02 21:31:00 -0600

    Defined TokenType and Token

--- src/parse.h
+++ src/parse.h
@@ -4,6 +4,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
+enum TokenType {
+    TOK_ID,
+    TOK_TEXT,
+    TOK_OPENING_OPTIONS_DELIMITER,
+    TOK_CLOSING_OPTIONS_DELIMITER,
+    TOK_EMPTY,
+};
+
+struct Token {
+    enum TokenType ttype;
+    char *tstr;
+    size_t tstr_max_len;
+};
 bool parse(FILE *file, struct Adventure *a);
 
 #endif // TEXT_ADVENTURES_PARSE
